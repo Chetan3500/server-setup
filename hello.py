503586d74383b9b9c9a25d415e1f6518c0baa15e
Chetan Tekam <tekamchetan3500@gmail.com>
2025-08-29 20:04:56 +0000

    Add Python adn FastAPI practice files

---
+++ hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print("Hello from Python!")
+name = "DevOps"
+print(f"Welcome, {name}!")
---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my FastAPI app!"}
+
+
+@app.get("/info")
+async def info():
+    return {"server": "EC2", "phase": "2"}
